e0aba2e3c4dc41efc32808d5f15b6f7d1f019818
riessnico <nicolauriess@gmail.com>
2021-06-17 16:50:07 -0300

    first version

---
+++ controllers/signin.js
@@ -0,0 +1,30 @@
+const handleSignin = (req, res, db, bcrypt) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json("incorret form submission");
+  }
+  db.select("email", "hash")
+    .where("email", "=", req.body.email)
+    .from("login")
+    .then((data) => {
+      const isValid = bcrypt.compareSync(password, data[0].hash);
+      if (isValid) {
+        return db
+          .select("*")
+          .from("users")
+          .where("email", "=", email)
+          .then((user) => {
+            console.log(user);
+            res.json(user[0]);
+          })
+          .catch((err) => res.status(400).json("unable to get user"));
+      } else {
+        res.status(400).json("wrong credentials");
+      }
+    })
+    .catch((err) => res.status(400).json("wrong credentials"));
+};
+
+module.exports = {
+  handleSignin: handleSignin,
+};
