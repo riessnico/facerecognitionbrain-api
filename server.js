458d69bfac32cb14c81457f886b06af29bef38b2
riessnico <nicolauriess@gmail.com>
2021-06-18 18:28:23 -0300

    code..]

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ const signin = require("./controllers/signin");
 const profile = require("./controllers/profile.js");
 
 const image = require("./controllers/image.js");
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 
 const db = knex({
   client: "pg",
