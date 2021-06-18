e0fe60d3976e0b191ac77845f3ed6dfdc1bd524c
riessnico <nicolauriess@gmail.com>
2021-06-17 23:31:00 -0300

    wrong code

--- controllers/register.js
+++ controllers/register.js
@@ -23,10 +23,10 @@ const handleRegister = (req, res, db, bcrypt) => {
           .then((user) => {
             res.json(user[0]);
           })
-          .catch((err) => res.status(400).json("unable to register"));
+          .then(trx.commit)
+          .catch(trx.rollback);
       })
-      .then(trx.commit)
-      .catch(trx.rollback);
+      .catch((err) => res.status(400).json("unable to register"));
   });
 };
 
