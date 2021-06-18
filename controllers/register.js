ed193b9714ea6b4627b0041eea8a34598aa4668a
riessnico <nicolauriess@gmail.com>
2021-06-17 23:40:35 -0300

    code..

--- controllers/register.js
+++ controllers/register.js
@@ -1,7 +1,7 @@
 const handleRegister = (req, res, db, bcrypt) => {
   const { email, name, password } = req.body;
   if (!email || !name || !password) {
-    return res.status(400).json("incorret form submission");
+    return res.status(400).json("incorrect form submission");
   }
   const hash = bcrypt.hashSync(password);
   db.transaction((trx) => {
@@ -22,12 +22,11 @@ const handleRegister = (req, res, db, bcrypt) => {
           })
           .then((user) => {
             res.json(user[0]);
-          })
-          .then(trx.commit)
-          .catch(trx.rollback);
+          });
       })
-      .catch((err) => res.status(400).json("unable to register"));
-  });
+      .then(trx.commit)
+      .catch(trx.rollback);
+  }).catch((err) => res.status(400).json("unable to register"));
 };
 
 module.exports = {
